ca845e4c8c27add5df746e3f115780f6fcfe5a2d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-29 14:20:35 -0400

    Create BcsPageBundle.php

---
+++ src/BcsPageBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Pages
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-pages
+ */
+
+namespace Bcs\PageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsPageBundle extends Bundle
+{
+}
