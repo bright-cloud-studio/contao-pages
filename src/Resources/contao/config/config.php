8a1263b93b498e6c93eb819bbc7294826af61622
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-29 14:37:07 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-glide
+ */
+
+  //$GLOBALS['TL_CTE']['glide']['glide_start'] = 'Bcs\GlideBundle\ContentGlideStart';
+  
+// Declare both of our new Content Elements as wrappers so we get the cool indenting in Content > Articles
+// $GLOBALS['TL_WRAPPERS']['start'][] = 'glide_start';
+// $GLOBALS['TL_WRAPPERS']['stop'][] = 'glide_stop';
+
+?>
