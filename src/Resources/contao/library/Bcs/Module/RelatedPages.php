9af76e2529b22cbd8a0922723d4eabd4468ea49d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-30 10:33:46 -0400

    Create RelatedPages.php

---
+++ src/Resources/contao/library/Bcs/Module/RelatedPages.php
@@ -0,0 +1,173 @@
+<?php
+ 
+/**
+ * Zyppy Page
+ *
+ * Copyright (C) 2018-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/zyppy_page
+ * @link       https://andrewstevens.consulting
+ */
+ 
+ 
+ 
+namespace Bcs\Module;
+
+use Contao\Config;
+use Contao\Environment;
+use Contao\FrontendTemplate;
+use Contao\ImageSizeModel;
+use Contao\PageModel;
+use Contao\FilesModel;
+use Contao\StringUtil;
+use Contao\System;
+use Contao\FrontendUser;
+
+
+class RelatedPages extends \Contao\Module
+{
+ 
+	/**
+	 * Template
+	 * @var string
+	 */
+	protected $strTemplate = 'mod_related_pages';
+
+
+	/**
+	 * Do not display the module if there are no menu items
+	 *
+	 * @return string
+	 */
+	public function generate()
+	{	
+		if (TL_MODE == 'BE')
+		{
+			/** @var \BackendTemplate|object $objTemplate */
+			$objTemplate = new \BackendTemplate('be_wildcard');
+
+			$objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['related_pages'][0]) . ' ###';
+			$objTemplate->title = $this->headline;
+			$objTemplate->id = $this->id;
+			$objTemplate->link = $this->name;
+			$objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+	
+			return $objTemplate->parse();
+		}	
+		$strBuffer = parent::generate();
+		return $strBuffer;
+	}
+
+
+	/**
+	 * Generate the module
+	 */
+	protected function compile()
+	{		
+		/** @var \PageModel $objPage */
+		global $objPage;
+		$objContainer = \System::getContainer();
+
+		$arrRelated = \StringUtil::deserialize($objPage->page_related, true);
+	
+		$arrPages = array();
+		foreach ($arrRelated as $intPage) {
+			$objRelatedPage = \PageModel::findByPk($intPage);
+			if ($objRelatedPage) {
+				$row = $objRelatedPage->row();
+				$row['url'] = $objRelatedPage->getFrontendUrl();
+				
+				
+
+				if ($row['page_image']) {
+					$strPhoto = '';
+					$uuid = \StringUtil::binToUuid($row['page_image']);
+					$objFile = \FilesModel::findByUuid($uuid);
+					if ($objFile) {
+						$strPhoto = $objFile->path;
+
+						$arrMeta = \StringUtil::deserialize($objFile->meta);
+
+						$staticUrl = $objContainer->get('contao.assets.files_context')->getStaticUrl();
+						$objPicture = $objContainer->get('contao.image.picture_factory')->create(
+							$objContainer->getParameter('kernel.project_dir') . '/' . $objFile->path, 
+							($this->preview_image_size ? $this->preview_image_size : null)
+						);
+
+						$arrPicture = array
+						(
+							'img' => $objPicture->getImg($objContainer->getParameter('kernel.project_dir'), $staticUrl),
+							'sources' => $objPicture->getSources($objContainer->getParameter('kernel.project_dir'), $staticUrl)
+						);
+
+						$arrPicture['alt'] = $arrMeta['page_image_title'];
+						$arrPicture['title'] = $arrMeta['page_image_title'];
+						if ($row['page_image_overwrite_meta']) {
+							if ($row['page_image_alt']) {
+								$arrPicture['alt'] = $row['page_image_alt'];
+							}
+							if ($row['page_image_title']) {
+								$arrPicture['title'] = $row['page_image_title'];
+							}
+						}
+						
+						$arrPicture['class'] = 'page_image';
+						$objPictureTemplate = new \FrontendTemplate('picture_default');
+						$objPictureTemplate->setData($arrPicture);
+						$row['page_image'] = $objPictureTemplate->parse();
+					}
+				}
+				
+				if ($row['page_images']) {
+					$arrFormatted = array();
+					$arrImages = \StringUtil::deserialize($row['page_images']);
+					foreach ($arrImages as $strImage) {
+						$strPhoto = '';
+						$uuid = \StringUtil::binToUuid($strImage);
+						$objFile = \FilesModel::findByUuid($uuid);
+						if ($objFile) {
+							$strPhoto = $objFile->path;
+
+							$arrMeta = \StringUtil::deserialize($objFile->meta);
+
+							$staticUrl = $objContainer->get('contao.assets.files_context')->getStaticUrl();
+							$objPicture = $objContainer->get('contao.image.picture_factory')->create(
+								$objContainer->getParameter('kernel.project_dir') . '/' . $objFile->path, 
+								($this->preview_image_size ? $this->preview_image_size : null)
+							);
+
+							$arrPicture = array
+							(
+								'img' => $objPicture->getImg($objContainer->getParameter('kernel.project_dir'), $staticUrl),
+								'sources' => $objPicture->getSources($objContainer->getParameter('kernel.project_dir'), $staticUrl)
+							);
+
+							$arrPicture['alt'] = $arrMeta['page_image_title'];
+							$arrPicture['title'] = $arrMeta['page_image_title'];
+							if ($row['page_image_overwrite_meta']) {
+								if ($row['page_image_alt']) {
+									$arrPicture['alt'] = $row['page_image_alt'];
+								}
+								if ($row['page_image_title']) {
+									$arrPicture['title'] = $row['page_image_title'];
+								}
+							}
+							
+							$arrPicture['class'] = 'page_image';
+							$objPictureTemplate = new \FrontendTemplate('picture_default');
+							$objPictureTemplate->setData($arrPicture);
+							$arrFormatted[] = $objPictureTemplate->parse();
+						}
+					}
+					$row['page_images'] = $arrFormatted;
+				}
+				
+				$arrPages[] = $row;
+			}
+		}
+
+		$this->Template->request = ampersand(\Environment::get('indexFreeRequest'));
+		$this->Template->pages = $arrPages;
+	}
+
+}
