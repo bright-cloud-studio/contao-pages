e27cf31f062543c7afb94afa4d4093051b6a7b5e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-30 11:12:10 -0400

    Create tl_module.php

---
+++ src/Resources/contao/languages/en/tl_module.php
@@ -0,0 +1,20 @@
+<?php
+ 
+/**
+ * @copyright	Bright Cloud Studio
+ * @author		Bright Cloud Studio
+ * @package	Contao Pages
+ * @license		LGPL-3.0+
+ * @see			https://github.com/bright-cloud-studio/contao-pages
+ **/
+ 
+/** Fields */
+$GLOBALS['TL_LANG']['tl_module']['preview_image_size']  = array('Image Size', 'Image size to use for images.');
+$GLOBALS['TL_LANG']['tl_module']['navHeaderTpl']        = array('Header template', 'Here you can select the navigation template for the header.');
+$GLOBALS['TL_LANG']['tl_module']['navBodyTpl']          = array('Body template', 'Here you can select the navigation template for the body.');
+$GLOBALS['TL_LANG']['tl_module']['navSubitemTpl']       = array('Sub-item template', 'Here you can select the navigation template for the subitems.');
+$GLOBALS['TL_LANG']['tl_module']['navSubitemLimit']     = array('Sub-item limit', 'The maximum number of sub-items to show.');
+$GLOBALS['TL_LANG']['tl_module']['bodyNumberOfActive']  = array('Number of Active', 'The maximum number of active items shown in the body.');
+
+/** Legends */
+$GLOBALS['TL_LANG']['tl_contest']['image_size_legend'] 		= 'Image Size Configuration';
