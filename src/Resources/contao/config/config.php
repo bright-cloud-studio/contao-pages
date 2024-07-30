d3b1be23f76b7421267eeca2376515398df836f8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-30 09:24:23 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+* @copyright  Bright Cliud Studio
+* @author     Bright Cloud Studio
+* @package    Contao Pages
+* @license    LGPL-3.0+
+* @see	       https://github.com/bright-cloud-studio/contao-pages
+*/
+
