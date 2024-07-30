533e91391ae69a4f9d2c28b6cedefc5a68b2bd31
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-30 10:44:42 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,10 +1,18 @@
 <?php
  
 /**
-* @copyright  Bright Cliud Studio
-* @author     Bright Cloud Studio
-* @package    Contao Pages
-* @license    LGPL-3.0+
-* @see	       https://github.com/bright-cloud-studio/contao-pages
-*/
+ * @copyright	Bright Cloud Studio
+ * @author		Bright Cloud Studio
+ * @package	Contao Pages
+ * @license		LGPL-3.0+
+ * @see			https://github.com/bright-cloud-studio/contao-pages
+ **/
 
+/** Front end modules */ 
+$GLOBALS['FE_MOD']['zyppy_page']['pagepreview'] 	= 'Bcs\Module\PagePreview'; 
+$GLOBALS['FE_MOD']['zyppy_page']['related_pages'] 	= 'Bcs\Module\RelatedPages'; 
+
+
+/**
+ \\\\e* Hooks */
+$GLOBALS['TL_HOOKS']['replaceInsertTags'][]				= array('Bcs\PagePreview', 'replaceInsertTags');
