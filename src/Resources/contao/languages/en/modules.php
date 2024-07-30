9429cc94453b03b554525e7263b42bcfa93b1cb8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-30 11:09:41 -0400

    Create modules.php

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,14 @@
+<?php
+ 
+/**
+ * @copyright	Bright Cloud Studio
+ * @author		Bright Cloud Studio
+ * @package	Contao Pages
+ * @license		LGPL-3.0+
+ * @see			https://github.com/bright-cloud-studio/contao-pages
+ **/
+
+/** Front end modules */
+$GLOBALS['TL_LANG']['FMD']['contao_pages'] 				= 'Contao Pages';  
+$GLOBALS['TL_LANG']['FMD']['pagepreview'] 				= array('Page Preview', 'Page preview navigation.');  
+$GLOBALS['TL_LANG']['FMD']['related_pages'] 			= array('Related Pages', 'Shows related pages.');  
