252565b2bb774f628677ddb3b1130af9bd0953fb
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-30 11:16:24 -0400

    Create tl_page.php

---
+++ src/Resources/contao/languages/en/tl_page.php
@@ -0,0 +1,29 @@
+<?php
+ 
+/**
+ * @copyright	Bright Cloud Studio
+ * @author		Bright Cloud Studio
+ * @package	Contao Pages
+ * @license		LGPL-3.0+
+ * @see			https://github.com/bright-cloud-studio/contao-pages
+ **/
+ 
+/** Fields */
+$GLOBALS['TL_LANG']['tl_page']['page_image']                 = array('Page image', 'Please select an image for this page.');
+$GLOBALS['TL_LANG']['tl_page']['page_image_size']            = array('Image Size', 'Here you can set the image dimensions and the resize mode.');
+$GLOBALS['TL_LANG']['tl_page']['page_image_margin']          = array('Image Margin', 'Here you can enter the top, right, bottom and left margin.');
+$GLOBALS['TL_LANG']['tl_page']['page_image_fullsize']        = array('Full-size View/New Window<', 'Open the full-size image in a lightbox or the link in a new browser window.');
+$GLOBALS['TL_LANG']['tl_page']['page_image_overwrite_meta']  = array('Overwrite Meta Data', 'Overwrite the meta data from the file manager.');
+$GLOBALS['TL_LANG']['tl_page']['page_image_alt']             = array('Alternate Text', 'Here you can enter an alternate text for the image (&lt;em&gt;alt&lt;/em&gt; attribute).');
+$GLOBALS['TL_LANG']['tl_page']['page_image_title']           = array('Image Title', 'Here you can enter the image title (&lt;em&gt;title&lt;/em&gt; attribute).');
+$GLOBALS['TL_LANG']['tl_page']['page_image_link']            = array('Image Link Target', 'A custom image link target will override the lightbox link, so the image cannot be viewed fullsize anymore.');
+$GLOBALS['TL_LANG']['tl_page']['page_image_caption']         = array('Image Caption', 'Here you can enter a short text that will be displayed below the image.');
+$GLOBALS['TL_LANG']['tl_page']['rich_text_1']                = array('Rich Text 1', 'Here you can enter text/HTML that that can be accessed by templates and insert tags (rich_text_1).');
+$GLOBALS['TL_LANG']['tl_page']['rich_text_2']                = array('Rich Text 2', 'Here you can enter text/HTML that that can be accessed by templates and insert tags (rich_text_2).');
+$GLOBALS['TL_LANG']['tl_page']['rich_text_3']                = array('Rich Text 3', 'Here you can enter text/HTML that that can be accessed by templates and insert tags (rich_text_3).');
+$GLOBALS['TL_LANG']['tl_page']['rich_text_4']                = array('Rich Text 4', 'Here you can enter text/HTML that that can be accessed by templates and insert tags (rich_text_4).');
+
+$GLOBALS['TL_LANG']['tl_page']['page_images']                = array('Page images', 'Allows for assigning multiple images to this page.');
+$GLOBALS['TL_LANG']['tl_page']['page_teaser']                = array('Page teaser', 'Please enter teaser text for this page.');
+$GLOBALS['TL_LANG']['tl_page']['page_image_url']             = array('Image URL', 'External image for this page.');
+$GLOBALS['TL_LANG']['tl_page']['page_related']               = array('Related pages', 'Please select any related pages.');
