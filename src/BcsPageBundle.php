32f167cb978102166296079a166e55b542301ef0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-30 10:14:52 -0400

    Update BcsPageBundle.php

--- src/BcsPageBundle.php
+++ src/BcsPageBundle.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * @copyright  Bright Cloud Studio
- * @author     Bright Cloud Studio
- * @package    Contao CE Glide
- * @license    LGPL-3.0+
- * @see	       https://github.com/bright-cloud-studio/contao-popups
- */
+ * @copyright	Bright Cloud Studio
+ * @author		Bright Cloud Studio
+ * @package	Contao Pages
+ * @license		LGPL-3.0+
+ * @see			https://github.com/bright-cloud-studio/contao-pages
+ **/
 
 namespace Bcs\PageBundle;
 
