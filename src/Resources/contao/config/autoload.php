a4b571f7286d832543dd32e0c4352b54eaf1bf99
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-07-30 13:20:23 -0400

    Update autoload.php

--- src/Resources/contao/config/autoload.php
+++ src/Resources/contao/config/autoload.php
@@ -1,32 +1,26 @@
 <?php
  
 /**
- * Zyppy Page
- *
- * Copyright (C) 2018-2022 Andrew Stevens Consulting
- *
- * @package    asconsulting/zyppy_page
- * @link       https://andrewstevens.consulting
- */
+ * @copyright	Bright Cloud Studio
+ * @author		Bright Cloud Studio
+ * @package	Contao Pages
+ * @license		LGPL-3.0+
+ * @see			https://github.com/bright-cloud-studio/contao-pages
+ **/
 
-
-/**
- * Register the classes
- */
+/** Register the classes */
 ClassLoader::addClasses(array
 (
-    'Bcs\Backend\ZyppyPage' 	=> 'system/modules/zyppy_page/library/Bcs/Backend/ContaoPages.php',
-    'Bcs\Module\PagePreview' 	=> 'system/modules/zyppy_page/library/Bcs/Module/PagePreview.php',
-    'Bcs\Module\RelatedPages' 	=> 'system/modules/zyppy_page/library/Bcs/Module/RelatedPages.php',
-	'Bce\PagePreview' 			=> 'system/modules/zyppy_page/library/Bcs/PagePreview.php'
+    //'Bcs\Backend\ContaoPages' 	=> 'system/modules/zyppy_page/library/Bcs/Backend/ContaoPages.php',
+    //'Bcs\Module\PagePreview' 	=> 'system/modules/zyppy_page/library/Bcs/Module/PagePreview.php',
+    //'Bcs\Module\RelatedPages' 	=> 'system/modules/zyppy_page/library/Bcs/Module/RelatedPages.php',
+	//'Bcs\PagePreview' 			=> 'system/modules/zyppy_page/library/Bcs/PagePreview.php'
 ));
 
-/**
- * Register the templates
- */
+/** Register the templates */
 TemplateLoader::addFiles(array
 (
-    'mod_pagepreview' 			=> 'system/modules/zyppy_page/templates',
-	'nav_pagepreview_header' 	=> 'system/modules/zyppy_page/templates',
-	'nav_pagepreview_body' 		=> 'system/modules/zyppy_page/templates',
+    //'mod_page_preview' 			=> 'system/modules/zyppy_page/templates',
+	//'nav_pagepreview_header' 	=> 'system/modules/zyppy_page/templates',
+	//'nav_pagepreview_body' 		=> 'system/modules/zyppy_page/templates',
 ));
